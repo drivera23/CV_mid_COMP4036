9d26dd6124208e6a87ed9d7f393fa9fa9c129160
drivera23 <60760172+drivera23@users.noreply.github.com>
2022-03-21 18:23:12 -0400

    looping through tasks

--- function.js
+++ function.js
@@ -1,19 +1,31 @@
 class WorkExperience{
     company;
     year;
+    tasks = ["Optimized data-gathering code using Ruby to examine thousands of server checkpoints and compliance standards.", 
+    "Wrote queries using PostgreSQL to connect multiple relations and further analyze servers and their data fields.",
+    "Developed a Power BI dashboard to demonstrate outliers and group/label data for future compliance updates."
+    ];
     constructor(company, year){
         this.company = company;
         this.year = year;
     }
-  
+
     speak(){
       console.log(this.company, this.year);
+      return
+    }
+
+    display_task(){
+      for (let i = 0; i < this.tasks.length; i++) {
+        console.log(this.tasks[i]) + "\n";
+      }
     }
     
 }
   
 let obj1 = new WorkExperience("Amazon", 2022);
 obj1.speak();
+obj1.display_task();
 
 let obj2 = new WorkExperience("AIG", 2021);
 obj2.speak();
