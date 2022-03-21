805cc99f697af32bb4d4ffb4caccb4814cb6e3d0
drivera23 <60760172+drivera23@users.noreply.github.com>
2022-03-21 17:37:45 -0400

    first_js_class

--- function.js
+++ function.js
@@ -1,1 +1,19 @@
+class WorkExperience{
+    company;
+    year;
+    constructor(company, year){
+        this.company = company;
+        this.year = year;
+    }
+  
+    speak(){
+      console.log(this.company, this.year);
+    }
+    
+}
+  
+let obj1 = new WorkExperience("Amazon", 2022);
+obj1.speak();
 
+let obj2 = new WorkExperience("AIG", 2021);
+obj2.speak();
