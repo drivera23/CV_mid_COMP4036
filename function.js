55f8904a9150ea033ea4234eb72e1ae8a18f4e83
drivera23 <60760172+drivera23@users.noreply.github.com>
2022-03-21 23:26:41 -0400

    basic html creation

--- function.js
+++ function.js
@@ -1,31 +1,51 @@
+class CV{
+
+}
+
 class WorkExperience{
     company;
     year;
-    tasks = ["Optimized data-gathering code using Ruby to examine thousands of server checkpoints and compliance standards.", 
-    "Wrote queries using PostgreSQL to connect multiple relations and further analyze servers and their data fields.",
-    "Developed a Power BI dashboard to demonstrate outliers and group/label data for future compliance updates."
-    ];
+    tasks = [];
     constructor(company, year){
         this.company = company;
         this.year = year;
     }
 
-    speak(){
-      console.log(this.company, this.year);
-      return
+    details(){
+      return this.company + this.year;
     }
 
-    display_task(){
+    add_task(task){
+      this.tasks.push(task);
+    }
+
+    display_task(tag, type){
       for (let i = 0; i < this.tasks.length; i++) {
-        console.log(this.tasks[i]) + "\n";
+          var item = document.createElement(type);
+          item.appendChild(document.createTextNode(this.tasks[i]))
+          tag.appendChild(item)
       }
+      return;
     }
-    
-}
-  
-let obj1 = new WorkExperience("Amazon", 2022);
-obj1.speak();
-obj1.display_task();
 
-let obj2 = new WorkExperience("AIG", 2021);
-obj2.speak();
+    to_HTML(){ // se crea el header, date, y objetivos
+      var tag = document.createElement("p"); // <p></p>
+      tag.style.cssText = 'margin-left: 25% width: 50%; ';
+      var ul = document.createElement("ul");
+      ul.style.cssText = 'margin-left: 25%; ';
+      this.display_task(ul, 'li');
+      tag.appendChild(ul)
+      var element = document.getElementsByTagName("body")[0];
+      element.appendChild(tag); // <body> <p>TEST TEXT</p> </body>
+    }
+
+}
+
+// Experience objects
+let obj = new WorkExperience("AIG", 2021);
+obj.add_task("Optimized data-gathering code using Ruby to examine thousands of server checkpoints and compliance standards");
+obj.add_task("Wrote queries using PostgreSQL to connect multiple relations and further analyze servers and their data fields.");
+obj.add_task("Developed a Power BI dashboard to demonstrate outliers and group/label data for future compliance updates.");
+
+obj.to_HTML();
+// Experience objects
